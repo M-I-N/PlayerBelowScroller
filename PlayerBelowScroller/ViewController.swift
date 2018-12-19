6a775c17598c18d4927a717e66debb75bbe1d615
Mufakkharul Islam Nayem <mufakkharul.nayem@bjitgroup.com>
2018-12-19 15:29:52 +0600

    Initial Commit

---
+++ PlayerBelowScroller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayerBelowScroller
+//
+//  Created by Nayem BJIT on 12/19/18.
+//  Copyright © 2018 BJIT Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
